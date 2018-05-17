2ec5df147301a80511efdbc99ef88932bf6f5e3f
prabhatbhattarai <talktofreeman@gmail.com>
2018-05-17 18:30:44 -0500

    First version of project

---
+++ ProjectBasedPythonCourse/01_HellowWorld/project.py
@@ -0,0 +1,18 @@
+#This is the first project in python. It implements basics of python
+#This file is built for python3
+
+#Basic printing:
+print ("###################################################")
+print ("Hello pythonist this is how you print in  python3 !")
+print ("###################################################")
+print ()
+
+#Ask for name and change pythonist value by your name.
+
+myName= input("Please print your name: ")
+
+#Now you can use the input value in string as follows:
+
+
+print ("Hello "+myName+" this is how you print in  python3 !")
+
